98c23bea7f535eca1c13b2e6cac2b62a05b01db7
praveenpkg8 <praveenpkg8@gmail.com>
2024-05-16 23:51:47 +0530

    Modifed: coint handling

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
 
 
 
 func _on_body_entered(body):
 	print("+1 coint")
 	game_manager.add_coin()
-	queue_free()
+	animation_player.play("pickup")
