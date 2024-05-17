065d98d9d6bf3e3f436255b6c664eb62624ec996
praveenpkg8 <praveenpkg8@gmail.com>
2024-05-17 12:06:29 +0530

    Modified: for testing

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -9,3 +9,15 @@ func _on_body_entered(body):
 	print("+1 coint")
 	game_manager.add_coin()
 	animation_player.play("pickup")
+
+
+func _on_area_entered(area):
+	print("+1 coint")
+	game_manager.add_coin()
+	animation_player.play("pickup")
+
+
+func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	print("+1 coint")
+	game_manager.add_coin()
+	animation_player.play("pickup")
